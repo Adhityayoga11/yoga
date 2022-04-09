26863a3a255b8d1b34d65d61f93cdebd099d21ed
Adhityayoga11 <adhitxxxxx@gmail.com>
2022-04-09 13:59:37 +0700

    percobaanyoga

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1> perkenalkan </h1>";
+        echo "nama saya Adhitya Yoga Wandana
+            saya tingal didaerah Jakarta
+            olahraga yang saya sukai adalah
+            bulutangkis";
+	}
+}
